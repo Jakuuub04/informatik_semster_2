759ac22ec86d51d48b2c53dc13075154d7b36675
Jakob <gehtkeinenwasan77@gmail.com>
2025-03-19 14:03:04 +0100

    add initial C/C++ build task and example program

---
+++ praktikumsdatein/test1.c
@@ -0,0 +1,19 @@
+/**********************************************************************\
+* Kurzbeschreibung: Uebung: 0.1; Punkte: 0
+*
+* Datum:       Autor:      Grund der Aenderung:
+* 01.10.2011   Name        Neuerstellung
+\**********************************************************************/
+#include <stdio.h>
+
+
+/*--- Funktionsdefinitionen ------------------------------------------*/
+int main( void )
+{
+   printf("ich!\n");
+   
+   
+   getchar(); /*Ggf. noetig um Fenster offen zu halten (unter Windows)*/
+   return 0;
+}
+
