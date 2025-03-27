e70907820a44bac51cec566ed1d71ae2b901ce33
Jakob <gehtkeinenwasan77@gmail.com>
2025-03-27 11:52:06 +0100

    finish 5.6

--- praktikumsdatein/stunde_2/5.6.1.c
+++ praktikumsdatein/stunde_2/5.6.1.c
@@ -2,6 +2,26 @@
 * Kurzbeschreibung: Aufgabe 5.6.1 mit der Berechnung von Ausdrücken
 *
 * Datum: Autor:
-* <<20.03.2025>> <<Jakob Hofmmann>>
+* <<27.03.2025>> <<Jakob Hofmmann>>
 *
 \**********************************************************************/
+
+#include <stdio.h>
+
+int main () {
+
+    unsigned short x = 0x12AB;
+    unsigned short vertauscht;
+
+    printf("Die Zahl im normalen Zustand lautet: %x\n", x);
+
+    vertauscht = (x >> 8) | ((x & 0x00FF) << 8);
+
+    printf("Die Zahl im vertauschtem Zustand lautet: %x\n", vertauscht);
+
+    return 0;
+}
+
+
+
+
