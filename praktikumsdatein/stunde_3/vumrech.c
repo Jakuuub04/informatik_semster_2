5f87d7a89d0fda227add427d07c9be1289c52e63
Jakob <gehtkeinenwasan77@gmail.com>
2025-04-07 21:44:58 +0200

    fixxed

--- praktikumsdatein/stunde_3/vumrech.c
+++ praktikumsdatein/stunde_3/vumrech.c
@@ -11,12 +11,12 @@ int main() {
     printf("Gib deine zurückgelegte Zeit in Sekunden (s) ein: ");
     scanf("%f", &zeit);
 
-    double v_ms = strecke / zeit; //angegeben in Meter die Sekunde
+    double v_ms = strecke / zeit; 
     double v_kmh = v_ms * MS_NACH_KMH;
     double v_mtag = v_ms * MS_NACH_MTAG;
     double v_kmtag = v_ms * MS_NACH_KMTAG;
 
-    printf("Die Geschwindigkeit beträgt %f m/s, %f km/h, %f m/Tag und %f km/Tag. \n", v_ms, v_kmh, v_mtag, v_kmtag);
+    printf("Die Geschwindigkeit beträgt %.2f m/s, %.2f km/h, %.2f m/Tag und %.2f km/Tag. \n", v_ms, v_kmh, v_mtag, v_kmtag);
 
     return 0;
 }
