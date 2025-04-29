acc4fad0a04ca9accf0763370536759c78915fe6
Jakob <gehtkeinenwasan77@gmail.com>
2025-04-29 16:16:57 +0200

    fix

--- praktikumsdatein/stunde_5/gruss.c
+++ praktikumsdatein/stunde_5/gruss.c
@@ -6,30 +6,31 @@ int main() {
     printf("Gib die Stunde der momentanen Uhrzeit ein: ");
     scanf("%d", &stundenzahl);
 
-    if (stundenzahl >= 23 || stundenzahl <= 5)
-        { 
-            printf("Gute Nacht!");
-        }
-    else if (stundenzahl >= 6 || stundenzahl <= 10)
-        {
-            printf("Guten Morgen!");
-        }
-    else if (stundenzahl >= 11 || stundenzahl <= 13)
-        {
-            printf("Mahlzeit!");
-        }
-    else if (stundenzahl >= 14 || stundenzahl <= 17)
-        {
-            printf("Schönen Nachmittag!");
-        }
-    else if (stundenzahl >= 18 || stundenzahl <= 22)
-        {
-            printf("Guten Abend!");
-        }
-    else if (stundenzahl  >= 24 || stundenzahl <= 0)
+    if (stundenzahl < 0 || stundenzahl > 23) 
         {
             printf("%d ist keine erlaubte Stunden-Angabe!", stundenzahl);
         } 
+    else if (stundenzahl >= 23 || stundenzahl <= 5)
+        { 
+            printf("Gute Nacht!");
+        }
+    else if (stundenzahl >= 6 && stundenzahl <= 10)
+        {
+            printf("Guten Morgen!");
+        }
+    else if (stundenzahl >= 11 && stundenzahl <= 13)
+        {
+            printf("Mahlzeit!");
+        }
+    else if (stundenzahl >= 14 && stundenzahl <= 17)
+        {
+            printf("Schönen Nachmittag!");
+        }
+    else if (stundenzahl >= 18 && stundenzahl <= 22)
+        {
+            printf("Guten Abend!");
+        }
+
 
     return 0;
 }
