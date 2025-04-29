8e79d2b70eb05835747c18e8d1e3a41b9d60e12f
Jakob <gehtkeinenwasan77@gmail.com>
2025-04-29 16:10:15 +0200

    fixx

--- praktikumsdatein/stunde_4/quersum.c
+++ praktikumsdatein/stunde_4/quersum.c
@@ -13,13 +13,11 @@ int main(void)
         return -1;
     }
 
-
     if (scanf("%d", &zahl2) != 1 || zahl2 < 0 || zahl2 >= 10)
     {
         printf("Fehlerhafte Eingabe, bitte Programm neustarten! ");
         return -1;
     }
-
 
     if (scanf("%d", &zahl3) != 1 || zahl3 < 0 || zahl3 >= 10)
     {
@@ -27,20 +25,17 @@ int main(void)
         return -1;
     }
 
-
     if (scanf("%d", &zahl4) != 1 || zahl4 < 0 || zahl4 >= 10)
     {
         printf("Fehlerhafte Eingabe, bitte Programm neustarten! ");
         return -1;
     }
 
-
     if (scanf("%d", &zahl5) != 1 || zahl5 < 0 || zahl5 >= 10)
     {
         printf("Fehlerhafte Eingabe, bitte Programm neustarten! ");
         return -1;
     }
-
 
     int quersumme;
 
--- praktikumsdatein/stunde_5/gruss.c
+++ praktikumsdatein/stunde_5/gruss.c
@@ -4,7 +4,7 @@ int main() {
 
     int stundenzahl;
     printf("Gib die Stunde der momentanen Uhrzeit ein: ");
-    scanf("%d", stundenzahl);
+    scanf("%d", &stundenzahl);
 
     if (stundenzahl >= 23 || stundenzahl <= 5)
         { 
@@ -12,8 +12,24 @@ int main() {
         }
     else if (stundenzahl >= 6 || stundenzahl <= 10)
         {
-                             printf("Guten Morgen!");
+            printf("Guten Morgen!");
         }
+    else if (stundenzahl >= 11 || stundenzahl <= 13)
+        {
+            printf("Mahlzeit!");
+        }
+    else if (stundenzahl >= 14 || stundenzahl <= 17)
+        {
+            printf("Schönen Nachmittag!");
+        }
+    else if (stundenzahl >= 18 || stundenzahl <= 22)
+        {
+            printf("Guten Abend!");
+        }
+    else if (stundenzahl  >= 24 || stundenzahl <= 0)
+        {
+            printf("%d ist keine erlaubte Stunden-Angabe!", stundenzahl);
+        } 
 
     return 0;
 }
