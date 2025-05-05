43da02086b3c4303da983ca823d22414e48802c0
Jakob <gehtkeinenwasan77@gmail.com>
2025-05-05 18:16:40 +0200

    Schaltjahr Added

---
+++ praktikumsdatein/stunde_5/schalt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int Jahr;
+    printf("Geben sie ein abzufragenes Jahr an: ");
+    scanf("%d", &Jahr);
+
+    if (Jahr % 4 != 0) {
+        printf("---> Kein Schaltjahr");
+    } else if (Jahr % 100 != 0) {
+        printf("---> Schaltjahr"); 
+    } else if (Jahr % 400 == 0) {
+        printf("---> Schaltjahr");
+    } else {
+        printf("---> Kein Schaltjahr");
+    }
+
+    return 0;
+
+}
