155c0b193fcca629bfa06fe7ca5d9b2cc512a490
Jakob <gehtkeinenwasan77@gmail.com>
2025-06-16 21:19:25 +0200

    fertich!!!

--- praktikumsdatein/stunde_12/tictac.c
+++ praktikumsdatein/stunde_12/tictac.c
@@ -4,21 +4,57 @@
 
 #define MAX 10
 
-void spielfeldzeichnen(char feldgroesse[MAX][MAX], int groesse) {
+void spielfeldzeichnen(char feld[MAX][MAX], int groesse) {
     for (int i = 0; i < groesse; i++) {
         for (int j = 0; j < groesse; j++) {
-            printf("%c ",feldgroesse[i][j]);
+            printf("%c ",feld[i][j]);
         }
         printf("\n");
     }
 }
 
+int wincheck(char feld[MAX][MAX], int groesse, char symbol) {
+    //zeilen
+    for (int i = 0; i < groesse; i++) {
+        for (int j = 0; j <= groesse - 3; j++) {
+            if (feld[i][j] == symbol && feld[i][j+1] == symbol && feld[i][j+2] == symbol)
+                return 1;
+        }
+    }
+
+    //spalte
+    for (int i = 0; i < groesse; i++) {
+        for (int j = 0; j <= groesse - 3; j++) {
+            if (feld[j][i] == symbol && feld[j+1][i] == symbol && feld[j+2][i] == symbol)
+                return 1;
+        }
+    }
+
+    //links oben -> rechts unten
+    for (int i = 0; i <= groesse - 3; i++) {
+        for (int j = 0; j <= groesse - 3; j++) {
+            if (feld[i][j] == symbol && feld[i+1][j+1] == symbol && feld[i+2][j+2] == symbol)
+                return 1;
+        }
+    }
+
+    // rechts oben -> links unten
+    for (int i = 0; i <= groesse - 3; i++) {
+        for (int j = 2; j < groesse; j++) {
+            if (feld[i][j] == symbol && feld[i+1][j-1] == symbol && feld[i+2][j-2] == symbol)
+                return 1;
+        }
+    }
+
+    return 0; //keiner gewinnt
+}
+
+
 int main() {
 
     char feldgroesse[MAX][MAX];
-    int groesse;
-    int zeile;
-    int spalte;
+    int spalte, zeile, spieler = 1, groesse, zuege; 
+    char symbol;
 
     printf("Spielfeldgroesse (mind. 3, maximal 10)? ");
     scanf("%d",&groesse);
@@ -28,33 +64,49 @@ int main() {
     }
     // do while würde auch funktinieren -> do printf.... -> while(groesse < 3 || groesse > 10)
 
+    // feld mit . füllen
     for (int i = 0; i < groesse; i++)
         for (int j = 0; j < groesse; j++)
             feldgroesse[i][j] = '.';
 
-
-
     while(1) {
 
         spielfeldzeichnen(feldgroesse, groesse);
-        
+        symbol = (spieler == 1) ? 'X' : 'O';
+
         //Spieler 1
-        do {
-            printf("Dein Zug, Spieler 1 (Zeile, Spalte)?");
-            scanf("%d %d",&zeile, &spalte);
-            if(zeile > groesse || spalte > groesse) {
-                printf("Zu gross");
+        while(1) {
+            printf("Dein Zug %c, Spieler %d (Zeile, Spalte)? ", symbol, spieler);
+            if(scanf("%d,%d", &zeile, &spalte) != 2) {
+                printf("..... Unerlaubter Spielzug\n");
+                continue;
+            }
+
+            zeile--, spalte--;
+
+            if(zeile < 0 || zeile > groesse || spalte < 0 || spalte > groesse) {
+                printf("..... Unerlaubter Spielzug\n");
+            } else if (feldgroesse[zeile][spalte] != '.') {
+                printf("..... Unerlaubter Spielzug\n");
+            } else {
+                feldgroesse[zeile][spalte] = symbol;
+                zuege++;
                 break;
             }
         }
 
-        //Spieler 2
+        if(zuege == groesse * groesse) {
+            spielfeldzeichnen(feldgroesse, groesse);
+            printf("Wir haben ein unentschieden!");
+            break;
+        }
+        if(zuege >= 5 && wincheck(feldgroesse, groesse, symbol)) {
+            spielfeldzeichnen(feldgroesse, groesse);
+            printf("Spieler %d hat gewonenn", spieler);
+            break;
+        }
 
-
+        spieler = 5 - 2 - spieler;
     }
-
-
-
-
     return 0;
 }
