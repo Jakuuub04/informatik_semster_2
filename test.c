a835cf225cc5d0069a6bf415e2acc32d922571f9
Jakob <jakob.jh.hofmann@outlook.de>
2025-07-11 12:17:09 +0200

    pruefungsvorbereitung

--- test.c
+++ test.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-#include <math.h>
-#define MWST 19
+#include <stdlib.h>
 
-int i, zahl = 0;
-int main() {
-    printf("zahl! ");
+int quadsum(int n) {
+
+    int erg = 0; int i;
+
+    if (n > 0) {
+        for (i = n; i > 1; i--) {
+            printf("%d + ", i*i);
+            erg += i*i;
+        }
+        printf("%d = ", i*i);
+        erg += i*i;
+        
+        return erg;
+    }
+}
+
+void main() {
+
+    int vari;
+    srand(time(NULL));
+
+    printf("eingabe: ", &vari);
+    scanf("%d", &vari);
+
+    printf("Okta %o\n", vari);
+    printf("Hexa: %x\n", vari);
+
+    printf("Dual: ");
     
-    if (scanf("%d", &zahl) != 1) {
-        printf("fehler");
-        return 1;
+    for (int i = sizeof(int)*8-1; i >= 0; i--) {
+        printf("%d", (vari >> i) & 0x1);
+    }
+    printf("\n");
+
+    int array[200];
+
+
+    //fuelle
+    for (int i = 0; i < vari; i++) {
+        array[i] = rand() % 101 + 100;
+    }
+    //rolle
+    if (vari > 0) {
+        int endwert = array[vari - 1];
+        for (int i = vari - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        array[0] = endwert;
     }
 
-    while(scanf("%d",&zahl) != 1) {
-        printf("fehler");
-        return 1;
-    }
-
-    printf("succsess");
-    return 0;
+    printf("%d", quadsum(vari));
+    
 }
